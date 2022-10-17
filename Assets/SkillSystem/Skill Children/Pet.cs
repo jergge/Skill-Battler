95cc4579a2a6721124ddfdcf4955552561f9189a
jergge <andrew.kerr.92@gmail.com>
2022-10-17 13:08:14 +1100

    More work on pets

--- Assets/SkillSystem/Skill Children/Pet.cs
+++ Assets/SkillSystem/Skill Children/Pet.cs
@@ -12,6 +12,21 @@ public class Pet : Skill, IActiveSkill
 
     public void Cast(Transform spawnLocation, TargetInfo targetInfo)
     {
+        if(OnCooldown())
+        {
+            return;
+        }
         summonedPet = GameObject.Instantiate<PetPrefab>(petToSummon, spawnLocation.position, Quaternion.identity);
+        summonedPet.master = source;
+
+        summonedPet.OnDeath += OnPetDeath;
+        ResetCooldown();
+        PauseCooldown();
+    }
+
+    void OnPetDeath()
+    {
+        summonedPet.OnDeath -= OnPetDeath;
+        ResumeCooldown();
     }
 }}
--- Assets/SkillSystem/Skill Children/PetPrefab.cs
+++ Assets/SkillSystem/Skill Children/PetPrefab.cs
@@ -1,8 +1,75 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PetPrefab : LivingEntity
 {
+    public Animator animator;
+    public GameObject master;
+    public float currentMoveSpeed;
+    public float maxSpeedWhileTurning;
+    public float turnSpeed = 1;
+    Vector3 currentMoveDirectionTarget;
+    Vector3 currentMoveDirection;
+    public float masterFollowRadius = 5f;
+    public Rigidbody rb;
+    float distanceToMaster => Vector3.Distance(transform.position, master.transform.position);
+    public enum PetState
+    {
+        follow,
+        stay,
+        explore,
+        attack,
+        dead
+    }
+
+    PetState petState = PetState.follow;
+
+    public event Action OnDeath;
+
+    new void Start()
+    {
+        base.Start();
+        StartCoroutine(Follow());
+    }
+
+    void Update()
+    {
+        TurnToFace();
+        animator.SetFloat("moveSpeed", currentMoveSpeed);
+    }
+
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + transform.forward * currentMoveSpeed * Time.fixedDeltaTime);
+
+    }
+
+    IEnumerator Follow()
+    {
+        while(true)
+        {
+            Debug.Log(distanceToMaster);
+            if (distanceToMaster <= masterFollowRadius)
+            {
+                currentMoveSpeed = 0;
+                Debug.Log("stopping");
+            } else {
+            Debug.Log("starting up again ");
+            currentMoveSpeed = 3;
+
+            }
+            //currentMoveDirectionTarget = master.transform.position.normalized;
+
+            yield return null;
+        }
+    }
+
+    public void TurnToFace()
+    {
+        transform.LookAt(new Vector3(master.transform.position.x, transform.position.y, master.transform.position.z));
+
+    }
 
 }
