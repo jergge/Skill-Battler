f3364141abf2382e55e031da32f5d24c2e276a5c
jergge <andrew.kerr.92@gmail.com>
2023-03-18 00:31:02 +1100

    To Global namespace, new ways to addcomponents

--- Assets/Systems/SkillSystem/Utilities/Extensions.cs
+++ Assets/Systems/SkillSystem/Utilities/Extensions.cs
@@ -1,42 +1,103 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using SkillSystem;
 using UnityEngine;
 
-namespace SkillSystem.Extensions {
-public static class Extensions
+public static partial class Extensions
 {
-    
-    public static T AddStackableComponent<T>(this GameObject gameObject, int count = 1) where T: MonoBehaviour, IStackableComponent
+    [Obsolete("Use the overloaded AddComponent<T> instead")]
+    public static T AddStackableComponent<T>(this GameObject gameObject) where T: MonoBehaviour, UniqueComponent<T>
     {
         T test;
         if (gameObject.TryGetComponent<T>(out test))
         {
-            test.AddStack(count);
+            test.AddNew();
             return test;
         } else{
             return gameObject.AddComponent<T>();
         }
     }
 
-    public static T AddComponent<T>(this GameObject gameObject, int count) where T: MonoBehaviour, IStackableComponent
+    [Obsolete("This is currently not working, do this manually at the moment")]
+    public static T AddComponent<T>(this GameObject gameObject) where T: UniqueComponent<T>
     {
         T test;
         if (gameObject.TryGetComponent<T>(out test))
         {
-            test.AddStack(count);
+            test.AddNew();
             return test;
         } else{
             return gameObject.AddComponent<T>();
         }
     }
 
+    [Obsolete("Call Configure(Skill skill) after adding component instead of using this")]
     public static T AddComponent<T>(this GameObject gameObject, Skill skill) where T: Buff
     {
-            T tempBuff = gameObject.AddComponent<T>();
+            T buff = gameObject.AddComponent<T>();
 
-            tempBuff.Configure(skill);
+            buff.Configure(skill);
 
-            return tempBuff;
+            return buff;
+    }
+
+    /// <summary>
+    /// Returns the Component if it is found on the GameObject, otherwise creates a new Component
+    /// </summary>
+    /// <typeparam name="C">The Component to search for / create</typeparam>
+    /// <param name="gameObject">the GameObject</param>
+    /// <returns>The component found or created</returns>
+    public static C AddOrGetComponent<C>(this GameObject gameObject) where C : MonoBehaviour
+    {
+        C test;
+        if (gameObject.TryGetComponent<C>(out test))
+        {
+            return test;
+        } else{
+            return gameObject.AddComponent<C>();
+        }
+    }
+
+    /// <summary>
+    /// Returns the Component if it is found on the GameObject, otherwise creates a new Component
+    /// </summary>
+    /// <typeparam name="C">The Component to search for / create</typeparam>
+    /// <param name="gameObject">the GameObject</param>
+    /// <param name="foundOnObject">Returns True if the component already existed</param>
+    /// <returns>The component found or created</returns>
+    public static C AddOrGetComponent<C>(this GameObject gameObject, out bool foundOnObject) where C : MonoBehaviour
+    {
+        C test;
+        if (gameObject.TryGetComponent<C>(out test))
+        {
+            foundOnObject = true;
+            return test;
+        } else{
+            foundOnObject = false;
+            return gameObject.AddComponent<C>();
+        }
+    }
+
+    /// <summary>
+    /// Returns the Component if it is found on the GameObject and the filter function returns True, otherwise creates a new Component
+    /// </summary>
+    /// <typeparam name="C">The Component to search for / create</typeparam>
+    /// <param name="gameObject">the GameObject</param>
+    /// <param name="filter">A function to search for a more specific existing Component of Type C</param>
+    /// <returns>The component found or created</returns>
+    public static C AddOrGetComponent<C>(this GameObject gameObject, Func<C, bool> filter) where C : MonoBehaviour
+    {
+        var components = gameObject.GetComponents<C>();
+        foreach (var component in components)
+        {
+            if ( filter(component) )
+            {
+                return component;
+            }
         }
 
-}}
+        return gameObject.AddComponent<C>();
+    }
+
+}
