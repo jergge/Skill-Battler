0e8952f167bfb0fafe2837f249c908861009faf0
jergge <andrew.kerr.92@gmail.com>
2023-03-20 00:35:30 +1100

    Can now push toasts

--- Assets/Scrpits/Player/Player.cs
+++ Assets/Scrpits/Player/Player.cs
@@ -3,17 +3,22 @@ using System.Collections;
 using System.Collections.Generic;
 using SkillSystem;
 using UnityEngine;
+using UnityEngine.InputSystem;
 using UnityEngine.UI;
 
 //The script for player controlled characters, now using the new Unity input system
 [RequireComponent(typeof(PlayerController), typeof(SkillManager), typeof(PlayerCamera))]
-[RequireComponent(typeof(TargetInfoSetter))]
-public class Player : LivingEntity
+[RequireComponent(typeof(TargetInfoSetter), typeof(PlayerInput))]
+public class Player : LivingEntity, ISendToastNotifications
 {
     public Camera playerCamera;
     public PlayerCamera playerCameraController; 
     public PlayerController playerController;
     public TargetInfoSetter targetInfoSetter;
+
+    public PlayerInput playerInput;
+
+    public event Action<ToastNotificationInfo> PushToast;
 
     //TargetInfo targetInfo = new TargetInfo();
     //public LayerMask targetInfoLayers;
